a2e4c8c8f148010689a22496db1603b06d45fc69
Ulisses Gurgel <sorjak1@gmail.com>
2012-07-26 01:50:34 -0700

    added entity managers

---
+++ cherry_strata.py
@@ -0,0 +1,35 @@
+import pygame, random, math
+from pygame.locals import *
+from utils_strata import *
+from globals_strata import *
+from basic_objects_strata import Static
+from plant_strata import Plant
+
+class Cherry(Plant):
+    def __init__(self, id, pos, game):
+        Plant.__init__(self, id, "cherries.png", self.spawn(pos), game, life=25)
+        self.selected = False
+        self.grazers = []
+        
+    def _update(self):
+        self.checkGrazers(self.game.creeps)
+        for g in self.grazers:
+            # g.eating = True
+            self.life -= .01
+                
+    def checkGrazers(self, grazers):
+        self.grazers = []
+        for g in grazers:
+            if self.rect.colliderect(g.rect) and g.nearestFood is self:
+                self.grazers.append(g)
+    
+    def _draw(self, screen): 
+        pass
+        
+    def _grow(self):
+        pass
+        
+    def spawn(self, pos):
+        if not pos:
+            pass
+    
